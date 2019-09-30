d877fc9e28c85fa4ff4c4c7365f83e8e9b50c7b4
김진우 <kjw2122@naver.com>
2019-09-30 21:18:02 +0900

    inputTest

---
+++ src/dataStructure/InputTest.java
@@ -0,0 +1,12 @@
+package dataStructure;
+
+import java.util.Scanner;
+
+public class InputTest {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        int input = stdIn.nextInt();
+        System.out.println(input);
+
+    }
+}
