32874d5e2de12364892f1ca8458a3deb33be08b1
김진우 <kjw2122@naver.com>
2020-03-10 13:36:20 +0900

    linked List JAVA

--- src/dataStructure/LinkedListJava.java
+++ src/dataStructure/LinkedListJava.java
@@ -3,5 +3,26 @@ package dataStructure;
 public class LinkedListJava {
     public static void main(String[] args) {
         LinkedList numbers = new LinkedList();
+        numbers.addFirst(10);
+        numbers.addFirst(99);
+        numbers.addFirst(28);
+        numbers.addFirst(55);
+        numbers.addFirst(32);
+        numbers.addLast(51);
+        numbers.addLast(76);
+        numbers.addLast(12);
+        numbers.addLast(21);
+
+        System.out.println(numbers.toString());
+        System.out.println("List's Size: " + numbers.size());
+
+        numbers.removeFirst();
+
+        numbers.remove(3);
+        numbers.remove(5);
+
+        System.out.println(numbers.toString());
+        System.out.println("List's Size: " + numbers.size());
+
     }
 }
