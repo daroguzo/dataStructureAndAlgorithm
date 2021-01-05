d413847ffacad7de042acc10203c8b1d79a964c7
daroguzo <kjw2122@naver.com>
2021-01-05 23:44:27 +0900

    ContainsDuplicate solution 2

--- src/dataStructure/ContainsDuplicate.java
+++ src/dataStructure/ContainsDuplicate.java
@@ -1,5 +1,7 @@
 package dataStructure;
 
+import java.util.Arrays;
+
 public class ContainsDuplicate {
     /*
     숫자로 구성된 배열이 주어졌을 때 그 배열에 중복된 숫자가 있는지 확인하는 함수를 작성하라. 중복된 숫자가 있다면 true 없다면 false.
@@ -8,16 +10,31 @@ public class ContainsDuplicate {
     예) 1 1 2 2 3 1 => true
 
      */
+
     public static void main(String[] args) {
         ContainsDuplicate containsDuplicate = new ContainsDuplicate();
-        System.out.println(containsDuplicate.solution1(new int[] {1, 2, 3, 4, 5, 6}));
+        System.out.println(containsDuplicate.solution2(new int[] {1, 2, 2, 4, 5, 5}));
     }
 
+    // 시간 복잡도: O(n²)
+    // 공간 복잡도: O(1)
     private boolean solution1(int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i + 1; j < numbers.length; j++) {
                 if (numbers[i] == numbers[j])
                     return true;
+            }
+        }
+        return false;
+    }
+
+    // 시간 복잡도: O(NlogN)
+    // 공간 복잡도: O(logN)
+    private boolean solution2(int[] numbers) {
+        Arrays.sort(numbers);
+        for (int i = 0; i < numbers.length - 1; i++) {
+            if (numbers[i] == numbers[i + 1]) {
+                return true;
             }
         }
         return false;
