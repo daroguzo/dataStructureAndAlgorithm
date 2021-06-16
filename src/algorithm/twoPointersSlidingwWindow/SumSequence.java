d8bb74be5dfaeabafc2c81be7454646d7d6a3819
daroguzo <kjw2122@naver.com>
2021-06-16 14:02:14 +0900

    연속된 자연수의 합(수학)

--- src/algorithm/twoPointersSlidingwWindow/SumSequence.java
+++ src/algorithm/twoPointersSlidingwWindow/SumSequence.java
@@ -10,10 +10,37 @@ public class SumSequence {
     }
 
     private int solution(int n) {
-        int answer = 0;
+        int answer = 0, lt = 0, m = n/2+1, sum = 0;
+        int[] arr = new int[m];
 
+        for (int i = 0; i < m; i++) {
+            arr[i] = i+1;
+        }
 
+        for (int rt = 0; rt < m; rt++) {
+            sum += arr[rt];
+            if (sum == n) {
+                answer++;
+            }
+            while (sum >= n) {
+                sum -= arr[lt++];
+                if (sum == n) {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
 
+    private int solution2(int n) {
+        int answer = 0, cnt = 1;
+        n--;
+        while (n > 0) {
+            cnt++;
+            n -= cnt;
+            if (n % cnt == 0)
+                answer++;
+        }
         return answer;
     }
 }
