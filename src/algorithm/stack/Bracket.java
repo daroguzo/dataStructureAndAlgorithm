db92d4b00ba6f9f6b2925e07dd83462f331a78a0
daroguzo <kjw2122@naver.com>
2021-06-23 11:59:30 +0900

    올바른 괄호

---
+++ src/algorithm/stack/Bracket.java
@@ -0,0 +1,38 @@
+package algorithm.stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Bracket {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(new Bracket().solution(input));
+    }
+
+    private String solution(String input) {
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+
+        for (char c :
+                input.toCharArray()) {
+            stack.push(c);
+        }
+
+        while (!stack.isEmpty()) {
+            if (stack.pop() == ')')
+                count++;
+            else
+                count--;
+
+            if (count < 0) {
+                return "NO";
+            }
+        }
+
+        if (count == 0)
+            return "YES";
+        else
+            return "NO";
+    }
+}
