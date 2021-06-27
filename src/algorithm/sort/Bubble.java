c9bac44d7d71f851f3b6131f99644a4fa53eda24
daroguzo <kjw2122@naver.com>
2021-06-27 21:30:27 +0900

    버블 정렬

---
+++ src/algorithm/sort/Bubble.java
@@ -0,0 +1,31 @@
+package algorithm.sort;
+
+import java.util.Scanner;
+
+public class Bubble {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        new Bubble().solution(n, arr);
+    }
+
+    private void solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+        for (int i :
+                arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
