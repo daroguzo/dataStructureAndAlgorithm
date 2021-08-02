67fc5e51fee371863899c9db1ed0cb8db78366ec
daroguzo <kjw2122@naver.com>
2021-08-02 15:40:56 +0900

    ascii 코드

---
+++ src/algorithm/etc/Q11654.java
@@ -0,0 +1,12 @@
+package algorithm.etc;
+
+import java.util.Scanner;
+
+public class Q11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        int answer = c;
+        System.out.println(answer);
+    }
+}
