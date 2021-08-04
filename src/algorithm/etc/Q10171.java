1fdd774b8ad8f5789a41d0e887ec8ec9f868c360
daroguzo <kjw2122@naver.com>
2021-08-04 13:52:47 +0900

    고양이

---
+++ src/algorithm/etc/Q10171.java
@@ -0,0 +1,10 @@
+package algorithm.etc;
+
+public class Q10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
