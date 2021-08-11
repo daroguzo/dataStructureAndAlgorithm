ee57dcac0f538f32025d2be7b461da90ae776d4a
daroguzo <kjw2122@naver.com>
2021-08-11 14:29:22 +0900

    정수 n개의 합

---
+++ src/algorithm/etc/Q15596.java
@@ -0,0 +1,12 @@
+package algorithm.etc;
+
+public class Q15596 {
+    long sum (int[] a) {
+        long answer = 0;
+        for (int i :
+                a) {
+            answer += i;
+        }
+        return answer;
+    }
+}
