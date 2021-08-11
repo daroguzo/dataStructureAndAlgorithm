1bcb664cee1148ee2455abf9ec2b55a1ab3ea260
daroguzo <kjw2122@naver.com>
2021-08-11 13:09:38 +0900

    빠른 A+B

---
+++ src/algorithm/etc/Q15552.java
@@ -0,0 +1,20 @@
+package algorithm.etc;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Q15552 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s = br.readLine();
+            StringTokenizer st = new StringTokenizer(s);
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(a + b) + "\n");
+        }
+        bw.flush();
+    }
+}
