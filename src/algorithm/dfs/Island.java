d8ba33b0a3bef4c307559b1c21b822a4146dbc2f
daroguzo <kjw2122@naver.com>
2021-08-13 12:19:14 +0900

    섬나라 아일랜드

---
+++ src/algorithm/dfs/Island.java
@@ -0,0 +1,44 @@
+package algorithm.dfs;
+
+import java.util.Scanner;
+
+public class Island {
+    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int[][] board;
+    static int answer = 0, n;
+    public void  dfs(int x, int y, int[][] board) {
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] == 1) {
+                board[nx][ny] = 0;
+                dfs(nx, ny, board);
+            }
+        }
+    }
+    public void solution(int[][] board) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    answer++;
+                    board[i][j] = 0;
+                    dfs(i, j, board);
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Island island = new Island();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        board = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+        island.solution(board);
+        System.out.println(answer);
+    }
+}
