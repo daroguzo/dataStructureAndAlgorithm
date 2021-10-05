67e7422377127c633d5e903d1f077e156175a82b
daroguzo <kjw2122@naver.com>
2021-10-05 21:02:08 +0900

    모의고사 Student클래스 만들어 풀어보기

--- src/algorithm/etc/PreTest.java
+++ src/algorithm/etc/PreTest.java
@@ -10,12 +10,29 @@ public class PreTest {
                 {2,1,2,3,2,4,2,5},
                 {3,3,1,1,2,2,4,4,5,5}
         };
+
+        Student[] students2 = {
+                new Student(new int[]{1,2,3,4,5}),
+                new Student(new int[]{2,1,2,3,2,4,2,5}),
+                new Student(new int[]{3,3,1,1,2,2,4,4,5,5})
+        };
         int[] answer1 = {1,2,3,4,5};
         int[] answer2 = {1,3,2,4,2};
 
         for (int i:
-             new PreTest().solution(answer2, students)) {
-            System.out.println(i + " ");
+             new PreTest().solution(answer1, students)) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i:
+                new PreTest().solution(answer1, students2)) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (Student s :
+                students2) {
+            System.out.print(s.solveCount + " ");
         }
     }
 
@@ -55,5 +72,47 @@ public class PreTest {
             }
         }
         return count;
+    }
+
+    public int[] solution(int[] answers, Student[] students) {
+        List<Integer> list = new ArrayList<>();
+        int[] answer = {};
+        int max = Integer.MIN_VALUE;
+
+        for (Student s : students) {
+            s.solve(answers);
+            if (max < s.solveCount)
+                max = s.solveCount;
+        }
+
+        for (int i = 0; i < students.length; i++) {
+            if (max == students[i].solveCount)
+                list.add(i + 1);
+        }
+
+        answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    static class Student {
+        int[] studentAnswers;
+        int answerLength;
+        int solveCount = 0;
+
+        public Student(int[] studentAnswers) {
+            this.studentAnswers = studentAnswers;
+            this.answerLength = studentAnswers.length;
+        }
+
+        public void solve(int[] answers) {
+            for (int i = 0; i < answers.length; i++) {
+                if (answers[i] == studentAnswers[i % answerLength]) {
+                    solveCount++;
+                }
+            }
+        }
     }
 }
