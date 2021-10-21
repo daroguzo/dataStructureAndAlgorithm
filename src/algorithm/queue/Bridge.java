c11a896235e51111e72e80d288407f531eb2794d
daroguzo <kjw2122@naver.com>
2021-10-21 11:34:37 +0900

    다리를 지나는 트럭

---
+++ src/algorithm/queue/Bridge.java
@@ -0,0 +1,35 @@
+package algorithm.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Bridge {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0, sum = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int truck :
+                truck_weights) {
+            while (true) {
+                if (queue.isEmpty()) {
+                    queue.offer(truck);
+                    sum += truck;
+                    answer += 1;
+                    break;
+                } else if (queue.size() == bridge_length) {
+                    sum -= queue.poll();
+                } else {
+                    if (sum + truck > weight) {
+                        queue.add(0);
+                        answer += 1;
+                    } else {
+                        queue.add(truck);
+                        sum += truck;
+                        answer += 1;
+                        break;
+                    }
+                }
+            }
+        }
+        return answer + bridge_length;
+    }
+}
