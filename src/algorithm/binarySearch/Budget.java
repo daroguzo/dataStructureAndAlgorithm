0d729cb64456be594e161440494e5256d945093a
daroguzo <kjw2122@naver.com>
2021-10-29 18:41:38 +0900

    Stream 사용

--- src/algorithm/binarySearch/Budget.java
+++ src/algorithm/binarySearch/Budget.java
@@ -1,21 +1,26 @@
 package algorithm.binarySearch;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Budget {
     public int solution(int[] budgets, int M) {
         int answer = 0, left = 0, right;
 
-        Arrays.sort(budgets);
-        right = budgets[budgets.length - 1];
+//        Arrays.sort(budgets);
+//        right = budgets[budgets.length - 1];
+
+        right = IntStream.of(budgets).max().orElse(0);
 
         while (left <= right) {
-            int mid = (left + right) / 2;
-            int sum = 0;
-            for (int b :
-                    budgets) {
-                sum += Math.min(b, mid);
-            }
+            final int mid = (left + right) / 2;
+            int sum = IntStream.of(budgets)
+                    .map(b -> Math.min(b, mid))
+                    .sum();
+//            for (int b :
+//                    budgets) {
+//                sum += Math.min(b, mid);
+//            }
             if (sum > M) {
                 right = mid - 1;
             } else {
