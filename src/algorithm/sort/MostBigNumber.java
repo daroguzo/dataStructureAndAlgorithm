43d81d5cb4624467afd0c4d2b764eb03eae8c263
daroguzo <kjw2122@naver.com>
2021-10-29 18:13:39 +0900

    startsWith함수 사용

--- src/algorithm/sort/MostBigNumber.java
+++ src/algorithm/sort/MostBigNumber.java
@@ -13,7 +13,7 @@ public class MostBigNumber {
 
         Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
 
-        if (arr[0].equals("0")) {
+        if (arr[0].startsWith("0")) {
             return "0";
         }
 
