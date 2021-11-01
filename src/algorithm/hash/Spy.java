905a6aed395dc52bef84993f86a4c269346d0a00
daroguzo <kjw2122@naver.com>
2021-11-01 23:01:28 +0900

    스파이

---
+++ src/algorithm/hash/Spy.java
@@ -0,0 +1,17 @@
+package algorithm.hash;
+
+import java.util.HashMap;
+
+public class Spy {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        for (String[] clothe : clothes) {
+            hashMap.put(clothe[1], hashMap.getOrDefault(clothe[1], 0) + 1);
+        }
+        for (String key : hashMap.keySet()) {
+           answer *= hashMap.get(key) + 1;
+        }
+        return answer - 1;
+    }
+}
