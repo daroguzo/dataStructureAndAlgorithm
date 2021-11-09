f82e16b19638553e17ed90808bebd89a6de24256
daroguzo <kjw2122@naver.com>
2021-11-09 19:33:46 +0900

    올바른 괄호 다른 풀이(Stack)

--- src/algorithm/dfs/PossibleBracket.java
+++ src/algorithm/dfs/PossibleBracket.java
@@ -1,5 +1,7 @@
 package algorithm.dfs;
 
+import java.util.Stack;
+
 public class PossibleBracket {
     int answer = 0;
     public int solution(int n) {
@@ -22,5 +24,36 @@ public class PossibleBracket {
 
         dfs(left + 1, right, n);
         dfs(left, right + 1, n);
+    }
+
+    static class P {
+        int open, close;
+
+        public P(int open, int close) {
+            this.open = open;
+            this.close = close;
+        }
+    }
+
+    public int solution2(int n) {
+        int answer = 0;
+
+        Stack<P> stack = new Stack<>();
+        stack.push(new P(0, 0));
+        while (!stack.isEmpty()) {
+            P p = stack.pop();
+
+            if (p.open > n) continue;
+            if (p.open < p.close) continue;
+            if (p.open + p.close == 2 * n) {
+                answer++;
+                continue;
+            }
+
+            stack.push(new P(p.open + 1, p.close));
+            stack.push(new P(p.open, p.close + 1));
+        }
+
+        return answer;
     }
 }
