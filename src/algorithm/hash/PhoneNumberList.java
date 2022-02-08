51cab82b40a84dbf5e1b47e56e3958087eaa8af0
daroguzo <kjw2122@naver.com>
2022-02-08 20:36:51 +0900

    전화번호 목록 startsWith() 활용

--- src/algorithm/hash/PhoneNumberList.java
+++ src/algorithm/hash/PhoneNumberList.java
@@ -1,5 +1,6 @@
 package algorithm.hash;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class PhoneNumberList {
@@ -7,7 +8,7 @@ public class PhoneNumberList {
         String[] phone_book = {"119", "97674223", "1195524421"};
         String[] phone_book2 = {"123","456","789"};
         String[] phone_book3 = {"12","123","1235","567","88"};
-        System.out.println(new PhoneNumberList().solution(phone_book3));
+        System.out.println(new PhoneNumberList().solution2(phone_book3));
     }
     public boolean solution(String[] phone_book) {
         boolean answer = true;
@@ -22,11 +23,24 @@ public class PhoneNumberList {
                 phone_book) {
             for (int i = 0; i < s.length(); i++) {
                 String tmp = s.substring(0, i);
-                if (hashMap.containsKey(tmp))
+                if (hashMap.containsKey(tmp)) {
                     answer = false;
+                    break;
+                }
             }
         }
 
         return answer;
+    }
+
+    public boolean solution2(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i]))
+                return false;
+        }
+
+        return true;
     }
 }
