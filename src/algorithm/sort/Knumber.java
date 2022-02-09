94dc91bc9b5300fc343e1408fd87f681b05bce17
daroguzo <kjw2122@naver.com>
2022-02-09 14:28:15 +0900

    K번째 수 수정 (sort)

--- src/algorithm/sort/Knumber.java
+++ src/algorithm/sort/Knumber.java
@@ -2,22 +2,34 @@ package algorithm.sort;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class Knumber {
+    public static void main(String[] args) {
+        int[] array = {1,5,2,6,3,7,4};
+        int[][] commands = {
+                {2,5,3},
+                {4,4,1},
+                {1,7,3}
+        };
+        Knumber knumber = new Knumber();
+        for (int i : knumber.solution(array, commands)) {
+            System.out.print(i + " ");
+        }
+    }
+
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
         ArrayList<Integer> sortedArr = new ArrayList<>();
         ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < commands.length; i++) {
-            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+        for (int[] command : commands) {
+            for (int j = command[0] - 1; j < command[1]; j++) {
                 sortedArr.add(array[j]);
             }
             Collections.sort(sortedArr);
-            System.out.println(sortedArr);
-            list.add(sortedArr.get(commands[i][2] - 1));
+            list.add(sortedArr.get(command[2] - 1));
             sortedArr.clear();
         }
-        System.out.println(list);
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
