8ee5dccbb41853f02094253be9f394414da08821
daroguzo <kjw2122@naver.com>
2022-02-10 13:19:20 +0900

    주식가격 (stack)

---
+++ src/algorithm/stack/StockPrice.java
@@ -0,0 +1,33 @@
+package algorithm.stack;
+
+import java.util.Stack;
+
+public class StockPrice {
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3};
+        StockPrice stockPrice = new StockPrice();
+        for (int i : stockPrice.solution(prices)) {
+            System.out.print(i + " ");
+        }
+    }
+
+    private int[] solution(int[] prices) {
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+            if (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
+                answer[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            answer[stack.peek()] = prices.length - stack.peek() - 1;
+            stack.pop();
+        }
+
+        return answer;
+    }
+}
